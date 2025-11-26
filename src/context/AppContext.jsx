32550a1c66e6e6f72d80aaeb5eb41b93d410773d
Gayan <gayanashoka38@gmail.com>
2025-11-27 01:08:16 +0530

    feat: implement Navbar and MainBanner components, add Home page, and set up AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,12 +2,15 @@ import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
+
 export const AppContextProvider =({children})=>{
+
     const navigate =useNavigate();
     const [user,setUser] =useState(null)
     const [isSeller,setIsSeller] =useState(false)
+    const [showUserLogin,setShowUserLogin] =useState(false)
     
-    const value ={navigate,user,setUser,setIsSeller,isSeller}
+    const value ={navigate,user,setUser,setIsSeller,isSeller,showUserLogin,setShowUserLogin}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
